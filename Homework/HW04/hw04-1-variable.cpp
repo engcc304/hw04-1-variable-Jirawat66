573465d0f40cdae8c8fec1ee7fe484821aa1a58d
jirawat malaiwan <tawan.jirawat@gmail.com>
2023-08-05 15:59:52 +0700

    1

--- Homework/HW04/hw04-1-variable.cpp
+++ Homework/HW04/hw04-1-variable.cpp
@@ -5,10 +5,11 @@
         10 20.0 Haha a
 */
 #include <stdio.h>
-
 int main() {
-int a = 10 ;
-float b = 20 ;
-char c[] = "Haha" ;
-char d = 'a' ;
-printf( "%d %0.1f %c %c ", a, b, c, d ) ; 
+    int a = 10 ;
+    float b = 20 ;
+    char c[] = "Haha" ;
+    char d = 'a' ;
+    printf( "%d %0.1f %s %c ",a,b,c,d ) ; 
+
+}
